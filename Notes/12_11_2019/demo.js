8c41aa70a7d1246aebfe07e307252d20b987d0b1
Joey Rundlett <rundlettjd@msoe.edu>
2019-12-11 14:12:04 -0600

    notes

---
+++ Notes/12_11_2019/demo.js
@@ -0,0 +1,29 @@
+// let i = new Array(6);
+// let j = [1, 3, 4, 5, 6];
+// let p = [1, 3, "3", 1, "fa"];
+// let z = i[4]
+//
+// class Rectangle {
+//     constructor(h, w) {
+//         this.height = h;
+//         this.width = w;
+//     }
+//
+//     getWidth() {
+//         return this.width;
+//     }
+//
+//     getHeight(){
+//         return this.height;
+//     }
+// }
+// console.log("Hello World!");
+// let message = prompt("Enter a Message");
+// alert("You entered: " + message);
+// let myRect = new Rectangle(3, 4);
+// let h = myRect.getHeight();
+
+let h = 3;
+let w = 4;
+let A = (h*w)/2;
+console.log(A);
